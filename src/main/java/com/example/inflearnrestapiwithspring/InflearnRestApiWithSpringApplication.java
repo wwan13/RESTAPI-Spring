7bed527a0b3ef6180ba396c19853de6bc9605adf
KimTaewan <wwan13@naver.com>
2021-03-14 23:40:39 +0900

    initialize project

---
+++ src/main/java/com/example/inflearnrestapiwithspring/InflearnRestApiWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.inflearnrestapiwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnRestApiWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnRestApiWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inflearnrestapiwithspring/InflearnRestApiWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inflearnrestapiwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnRestApiWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
