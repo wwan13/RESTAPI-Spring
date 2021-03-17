736fb79a8fc2393653e9be13c12a595de759a7af
KimTaewan <wwan13@naver.com>
2021-03-17 22:57:32 +0900

    add model mapper in spring bean

--- src/main/java/com/example/inflearnrestapiwithspring/InflearnRestApiWithSpringApplication.java
+++ src/main/java/com/example/inflearnrestapiwithspring/InflearnRestApiWithSpringApplication.java
@@ -1,13 +1,21 @@
 package com.example.inflearnrestapiwithspring;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InflearnRestApiWithSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InflearnRestApiWithSpringApplication.class, args);
+	}
+
+	// 공용으로 사용 가능 하기 때문에 bean에 등록해서 사용
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
