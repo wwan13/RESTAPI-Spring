aaa01ef53de5cfe7b971633b85ba65a60ba50a67
KimTaewan <wwan13@naver.com>
2021-03-18 15:00:05 +0900

    add test about bad request

--- src/test/java/com/example/inflearnrestapiwithspring/events/EventControllerTests.java
+++ src/test/java/com/example/inflearnrestapiwithspring/events/EventControllerTests.java
@@ -33,7 +33,7 @@ public class EventControllerTests {
     @Test
     public void createEvent() throws Exception {
 
-        Event event = Event.builder()
+        EventDto event = EventDto.builder()
                 .name("spring")
                 .description("REST API DEVELOPMENT WITH SPRING")
                 .beginEnrollmentDateTime(LocalDateTime.of(2020,12,23,14,21))
@@ -54,6 +54,34 @@ public class EventControllerTests {
                 .andDo(print())
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("id").exists());
+    }
+
+    @Test
+    public void createEvent_BadRequest() throws Exception {
+
+        Event event = Event.builder()
+                .id(100)
+                .name("spring")
+                .description("REST API DEVELOPMENT WITH SPRING")
+                .beginEnrollmentDateTime(LocalDateTime.of(2020,12,23,14,21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020,12,24,14,21))
+                .beginEventDateTime(LocalDateTime.of(2020,12,25,14,21))
+                .endEventDateTime(LocalDateTime.of(2020,12,26,14,21))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("우리집")
+                .free(true)
+                .offline(false)
+                .eventStatus(EventStatus.PUBLISHED)
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 
 }
